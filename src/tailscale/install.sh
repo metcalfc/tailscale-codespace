7d3feeeae0bed7073d3487ec3da71da0fd770f95
Chad Metcalf <metcalfc@gmail.com>
2023-09-26 23:24:05 +0000

    Adding a devcontainer, to develop the devcontainer feature.
    * Build wget/curl support into the feature.
    * Adding a basic test. We check that tailscale is installed, in the
        PATH, and executable.
    
    Previously the feature failed if you didn't have curl/wget installed.
    We added a clean_download function that will temporarily install wget
    if curl/wget isn't present. At the end of the run the script will take
    the packages back out which should minimize the layer size.
    
    If the base devcontainer already has curl/wget we'll use those.
    
    This also sets up being able to support Alpine images in the future.

--- src/tailscale/install.sh
+++ src/tailscale/install.sh
@@ -5,26 +5,124 @@
 
 set -euo pipefail
 
-tailscale_url="https://pkgs.tailscale.com/stable/tailscale_${VERSION}_amd64.tgz"
+case $(uname -m) in
+x86_64 | amd64)
+  ARCH=amd64
+  ;;
+arm64 | aarch64)
+  ARCH=arm64
+  ;;
+*)
+  echo "Unsupported architecture: $(uname -m)" >&2
+  exit 1
+  ;;
+esac
 
-download() {
-  if command -v curl &> /dev/null; then
-    curl -fsSL "$1"
-  elif command -v wget &> /dev/null; then
-    wget -qO - "$1"
-  else
-    echo "Must install curl or wget to download $1" 1&>2
-    return 1
-  fi
+clean_download() {
+    # Credit to Daniel Braun (danielbraun89) @ https://github.com/devcontainers-contrib/features
+    # The purpose of this function is to download a file with minimal impact on container layer size
+    # this means if no valid downloader is found (curl or wget) then we install a downloader (currently wget) in a
+    # temporary manner, and making sure to
+    # 1. uninstall the downloader at the return of the function
+    # 2. revert back any changes to the package installer database/cache (for example apt-get lists)
+    # The above steps will minimize the leftovers being created while installing the downloader
+    # Supported distros:
+    #  debian/ubuntu/alpine
+
+    url=$1
+    output_location=$2
+    tempdir=$(mktemp -d)
+    downloader_installed=""
+
+    _apt_get_install() {
+        tempdir=$1
+
+        # copy current state of apt list - in order to revert back later (minimize contianer layer size)
+        cp -p -R /var/lib/apt/lists $tempdir
+        apt-get update -y
+        apt-get -y install --no-install-recommends wget ca-certificates
+    }
+
+    _apt_get_cleanup() {
+        tempdir=$1
+
+        echo "removing wget"
+        apt-get -y purge wget --auto-remove
+
+        echo "revert back apt lists"
+        rm -rf /var/lib/apt/lists/*
+        rm -r /var/lib/apt/lists && mv $tempdir/lists /var/lib/apt/lists
+    }
+
+    _apk_install() {
+        tempdir=$1
+        # copy current state of apk cache - in order to revert back later (minimize contianer layer size)
+        cp -p -R /var/cache/apk $tempdir
+
+        apk add --no-cache  wget
+    }
+
+    _apk_cleanup() {
+        tempdir=$1
+
+        echo "removing wget"
+        apk del wget
+    }
+    # try to use either wget or curl if one of them already installer
+    if type curl >/dev/null 2>&1; then
+        downloader=curl
+    elif type wget >/dev/null 2>&1; then
+        downloader=wget
+    else
+        downloader=""
+    fi
+
+    # in case none of them is installed, install wget temporarly
+    if [ -z $downloader ] ; then
+        if [ -x "/usr/bin/apt-get" ] ; then
+            _apt_get_install $tempdir
+        elif [ -x "/sbin/apk" ] ; then
+            _apk_install $tempdir
+        else
+            echo "distro not supported"
+            exit 1
+        fi
+        downloader="wget"
+        downloader_installed="true"
+    fi
+
+    if [ $downloader = "wget" ] ; then
+        wget -q $url -O $output_location
+    else
+        curl -sfL $url -o $output_location
+    fi
+
+    # NOTE: the cleanup procedure was not implemented using `trap X RETURN` only because
+    # alpine lack bash, and RETURN is not a valid signal under sh shell
+    if ! [ -z $downloader_installed  ] ; then
+        if [ -x "/usr/bin/apt-get" ] ; then
+            _apt_get_cleanup $tempdir
+        elif [ -x "/sbin/apk" ] ; then
+            _apk_cleanup $tempdir
+        else
+            echo "distro not supported"
+            exit 1
+        fi
+    fi
+
 }
+
+tailscale_url="https://pkgs.tailscale.com/stable/tailscale_${VERSION}_${ARCH}.tgz"
+echo "Downloading: $tailscale_url"
 
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 scratch_dir="/tmp/tailscale"
+scratch_file="/tmp/tailscale.tgz"
 mkdir -p "$scratch_dir"
-trap 'rm -rf "$scratch_dir"' EXIT
+trap 'rm -rf "$scratch_dir $scratch_file"' EXIT
 
-download "$tailscale_url" |
-  tar -xzf - --strip-components=1 -C "$scratch_dir"
+clean_download "$tailscale_url" "$scratch_file"
+tar --strip-components=1 -C "$scratch_dir" -xzvf  "$scratch_file"
 install "$scratch_dir/tailscale" /usr/local/bin/tailscale
 install "$scratch_dir/tailscaled" /usr/local/sbin/tailscaled
 install "$script_dir/tailscaled-entrypoint.sh" /usr/local/sbin/tailscaled-entrypoint
